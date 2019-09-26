104b282d27696d705a0ed7008ee8f4295c79f101
colin <colinnoahpeter@gmail.com>
2019-09-27 01:56:13 +0800

    cleaned up console

--- src/MovieFeed.js
+++ src/MovieFeed.js
@@ -60,15 +60,15 @@ class MovieFeed extends React.Component {
                 <div className="wallpaper">
                     {feed ? feed.map(elem => {
                         return (
-                            <Container fluid={true} className="my-3">
-                                <Row className="d-block" onScroll={this.handleScroll}>
+                            <Container fluid={true} className="my-3" key={elem['row_name']}>
+                                <Row className="d-block">
                                     <h2 className="text-light category-heading">{elem['row_name']}</h2>
                                     <div className="super">
                                         <div className="parent">
                                             {elem['data'] ?
                                                 elem['data'].map(subelem => {
                                                     return (
-                                                        <Card className="shadow m-1 rounded" onClick={this.handleClick}>
+                                                        <Card className="shadow m-1 rounded" onClick={this.handleClick} key={subelem.id}>
                                                             <Link to={`/titles/${subelem.id}`}><CardImg top src={subelem.images[0].url} alt="" /></Link>
                                                             <CardBody>
                                                                 <CardTitle className="h6">{subelem.title}</CardTitle>
